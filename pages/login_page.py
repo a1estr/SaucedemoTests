0f93a8c05aa72a1befe62c0343834b16c966bf0a
Aliaksei Strakhau <straxov.9942@gmail.com>
2025-02-08 21:42:08 +0300

    HW22\Writing test for adding item to cart

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,6 +7,7 @@ class LoginPage(BasePage):
     PASSWORD_INPUT = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
     ERROR_MESSAGE = (By.CLASS_NAME, "error-message-container")
+    ERROR_BUTTON_INPUT_USERNAME = (By.XPATH, '//*[@id="login_button_container"]/div/form/div[1]/svg')
 
     def enter_username(self, username):
         self.enter_text(self.USERNAME_INPUT, username)
@@ -17,5 +18,17 @@ class LoginPage(BasePage):
     def click_login(self):
         self.click_element(self.LOGIN_BUTTON)
 
+    def find_error_button(self):
+        return self.find_element(self.ERROR_BUTTON_INPUT_USERNAME).text
+
     def get_error_message(self):
         return self.find_element(self.ERROR_MESSAGE).text
+
+    def get_login_page(self):
+        return self.open_url("https://www.saucedemo.com/")
+
+    def valid_login(self):
+        self.get_login_page()
+        self.enter_username("standard_user")
+        self.enter_password("secret_sauce")
+        self.click_login()
